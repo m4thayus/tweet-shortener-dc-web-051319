b73af3b41c7834edec766c8a6141d36f048c94b7
Matt Williams <matt.seongjun@gmail.com>
2019-04-01 21:14:43 -0400

    Rein in some tweets.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def shorten?(word)
+    dictionary = {
+        "hello" => "hi",
+        "to" => "2",
+        "two" => "2",
+        "too" => "2",
+        "for" => "4",
+        "four" => "4",
+        "be" => "b",
+        "you" => "u",
+        "at" => "@",
+        "and" => "&"
+    }
+    return dictionary.has_key?(word.downcase) ? dictionary[word.downcase] : word
+end
+
+def word_substituter(tweet)
+    tweet.split.map{ |w| shorten?(w) }.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+    tweets.each do |tweet| 
+        puts word_substituter(tweet)
+    end
+end
+
+def selective_tweet_shortener(tweet)
+    tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+    tweet.length > 140 ? short_tweet = word_substituter(tweet) : short_tweet = tweet
+    short_tweet.length > 140 ? tweet = "#{short_tweet.slice(0,137)}..." : tweet 
+end
